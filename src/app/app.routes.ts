75fa1a0d4e485ac06253ea019d462dfe06988099
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-01 09:52:05 +0300

    initial structure, assests content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./pages/profile/profile.component').then(
+        (c) => c.ProfileComponent
+      ),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        (c) => c.ProjectsComponent
+      ),
+  },
+  {
+    path: 'contacts',
+    loadComponent: () =>
+      import('./pages/contacts/contacts.component').then(
+        (c) => c.ContactsComponent
+      ),
+  },
+];
