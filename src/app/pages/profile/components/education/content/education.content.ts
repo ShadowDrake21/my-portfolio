1865e5cd501ee10ca4eb0911f237e10cab0cc350
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-04 00:10:36 +0300

    working experience and education

---
+++ src/app/pages/profile/components/education/content/education.content.ts
@@ -0,0 +1,10 @@
+import { IEducationItem } from '../models/education.model';
+
+export const educationContent: IEducationItem[] = [
+  {
+    studyName: "Bachelor's Degree in Software Engineering",
+    universityName: 'Chernihiv Polytechnic National University',
+    studyYears: '2021 - 2025',
+    yearsToFinish: 1,
+  },
+];
--- src/app/pages/profile/components/working-experience/working-experience.component.ts
+++ src/app/pages/profile/components/working-experience/working-experience.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { workingExperienceContent } from './content/working-experience.content';
 
 @Component({
   selector: 'app-working-experience',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './working-experience.component.html',
-  styleUrl: './working-experience.component.css'
+  styleUrl: './working-experience.component.css',
 })
 export class WorkingExperienceComponent {
-
+  workingExperienceContent = workingExperienceContent;
 }
