5729021558e41e8ad9f3cb3a5b21e75949b2dcbc
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-04 23:04:53 +0300

    contact-me: part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { FooterComponent } from '@shared/components/footer/footer.component';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,27 @@ import { FooterComponent } from '@shared/components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  private router = inject(Router);
+
+  isContactMePage: boolean = true;
+
+  private destroy$$: Subject<void> = new Subject<void>();
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(takeUntil(this.destroy$$))
+      .subscribe((event: any) => {
+        if (event instanceof NavigationEnd) {
+          if (this.router.url === '/contact-me') {
+            this.isContactMePage = false;
+          }
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
+  }
+}
