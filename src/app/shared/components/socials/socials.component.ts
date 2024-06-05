99a0233675189244d55cfd46f5606491f0afbe43
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-05 14:30:56 +0300

    theme-mode: part 2

--- src/app/shared/components/socials/socials.component.ts
+++ src/app/shared/components/socials/socials.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ThemeModeType } from '@shared/models/themeMode.model';
 
 @Component({
   selector: 'app-socials',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './socials.component.html',
-  styleUrl: './socials.component.css'
+  styleUrl: './socials.component.css',
 })
-export class SocialsComponent {
-
-}
+export class SocialsComponent {}
---
+++ src/app/store/application/application.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from '@ngrx/store';
+import { ThemeModeType } from '@shared/models/themeMode.model';
+
+export const loadThemeMode = createAction('[Application] LoadThemeMode');
+export const loadThemeModeSuccess = createAction(
+  '[Application] LoadThemeModeSuccess',
+  props<{ themeMode: ThemeModeType }>()
+);
+export const loadThemeModeFailure = createAction(
+  '[Application] LoadThemeModeFailure'
+);
+
+export const setThemeMode = createAction(
+  '[Application] SetThemeMode',
+  props<{ themeMode: ThemeModeType }>()
+);
