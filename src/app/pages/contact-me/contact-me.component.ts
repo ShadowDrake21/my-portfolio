47b29ba10040e59279e875a1a08cdfcb4cb62d80
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-06 12:22:02 +0300

    theme-mode: part 7 (contact: done)

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-
+import { Store } from '@ngrx/store';
+import { ApplicationState } from '@store/application/application.reducer';
+import * as ApplicationSelectors from '@store/application/application.selectors';
+import { Observable } from 'rxjs';
+import { ThemeModeType } from '@shared/models/themeMode.model';
 @Component({
   selector: 'app-contact-me',
   standalone: true,
@@ -9,4 +13,12 @@ import { ContactFormComponent } from './components/contact-form/contact-form.com
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.css',
 })
-export class ContactMeComponent {}
+export class ContactMeComponent implements OnInit {
+  private store = inject(Store<ApplicationState>);
+
+  themeMode$!: Observable<ThemeModeType | null>;
+
+  ngOnInit(): void {
+    this.themeMode$ = this.store.select(ApplicationSelectors.selectThemeMode);
+  }
+}
