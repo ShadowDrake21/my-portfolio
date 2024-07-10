15caad8af13565ef3cb1300865c6ba015863d044
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-10 12:33:26 +0300

    work experience

--- src/app/pages/profile/components/working-experience/content/working-experience.content.ts
+++ src/app/pages/profile/components/working-experience/content/working-experience.content.ts
@@ -1,3 +1,9 @@
 import { IWorkingExperience } from '../models/working-experience.model';
 
-export const workingExperienceContent: IWorkingExperience[] = [];
+export const workingExperienceContent: IWorkingExperience[] = [
+  {
+    positionName: 'Freelance',
+    companyName: 'Freelance',
+    workYears: '2023-2024',
+  },
+];
